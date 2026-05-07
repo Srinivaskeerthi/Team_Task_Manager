ed3a443cdf75d127e59dd4259cb1b2a8aa08886b
Srinivas Keerthi <srinivaskeerthi0710@gmail.com>
2026-05-07 18:01:01 +0530

    Fix frontend root route

--- server/server.js
+++ server/server.js
@@ -74,7 +74,7 @@ const PORT = process.env.PORT || 5000;
 // Serve frontend
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 server.listen(PORT, () => {
