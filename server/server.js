e2660e8b3a3ba4a61fbc81c084f74df41448910d
Srinivas Keerthi <srinivaskeerthi0710@gmail.com>
2026-05-07 17:27:09 +0530

    Fixed production CORS

--- server/server.js
+++ server/server.js
@@ -35,7 +35,9 @@ initSocket(server);
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL
+    ? process.env.CLIENT_URL.split(',')
+    : ['http://localhost:5173'],
   credentials: true,
 }));
 app.use(helmet({ crossOriginResourcePolicy: false }));
